ee1470c504ee0f97b2658041c828b50efc4ddca1
patudom <pudompra@cfa.harvard.edu>
2026-08-26 13:11:25 -0400

    Add text about Roman

---
+++ src/components/ScienceInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3 class="user-guide-header mt-5">
+    About Roman
+  </h3>
+  <p>
+    On August 30, 2026, NASA and SpaceX will launch the Nancy Grace Roman Space Telescope to L2, a location in space about 4 times farther away than the Moon!
+  </p>
+  <p>
+    Roman will take pictures of the sky using infrared light, which has less energy than the light we see with our eyes.
+  </p>
+  <h4 class="mt-5">
+    Who was Nancy Grace Roman?
+  </h4>
+  <p>
+    Nancy Grace Roman was an American astronomer and NASA's first chief astronomer starting in 1959. Roman essentially invented NASA's astronomy program from scratch, pushed for space astronomy, and helped develop pointing systems for space observatories. Her efforts led to Congress funding the Hubble Space Telescope project in 1977 and earned her the nickname "Mother of Hubble." Roman was so influential that NASA named this next generation observatory in her honor.
+  </p>
+  <h4 class="mt-5">
+    What will the Nancy Grace Roman Space Telescope do?
+  </h4>
+  <p>
+    Roman is designed to help astronomers answer essential questions about dark energy, exoplanets, and infrared astrophysics. It is built to complement Hubble and the James Webb Space Telescope (Webb) rather than replace them. 
+  </p>
+  <p>
+    Roman has a 2.4-meter primary mirror (the same size as Hubble's), and its resolution will be the same as Hubble's. A 300-megapixel infrared camera will allow it to image about 100 times the area of a single Hubble image.
+  </p>
+  <p>
+    This app lets you compare how much of the sky Roman, Hubble, and Webb can each see at once to help you understand Roman's capabilities.
+  </p>
+</template>
